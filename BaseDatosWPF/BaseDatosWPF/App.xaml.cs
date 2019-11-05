89f1dc3871eac4b11e7dfaac723924a293f0e53d
Alumno <Alumno@PC11>
2019-11-05 10:17:16 -0600

    Codigo terminado

---
+++ BaseDatosWPF/BaseDatosWPF/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BaseDatosWPF
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static string databaseName = "Contactos.db"; //damos el nombre de la base de datos
+        static string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string databasePath = System.IO.Path.Combine(folderPath, databaseName); //le decimos donde esta
+    }
+}
---
+++ BaseDatosWPF/BaseDatosWPF/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BaseDatosWPF.Clases; //Agregamos dos librerias
+using SQLite;
+
+namespace BaseDatosWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            Contactos contacto = new Contactos() //En esta variable se guardarán los siguientes datos
+            {
+                Nombre = txtNombre.Text,
+                Correo = txtCorreo.Text,
+                Telefono = txtTelefono.Text,
+            };
+            using(SQLiteConnection conexion = new SQLiteConnection(App.databasePath)) //creamos una conexion de tipo sql y se guardará en app.databasePath
+            {
+                conexion.CreateTable<Contactos>();
+                conexion.Insert(contacto);
+            }
+            Close();
+        }
+    }
+}
